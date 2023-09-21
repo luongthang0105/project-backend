9147246d4ce78305c81e62e4e33962da9d204517
Mutsuki <z5400771@ad.unsw.edu.au>
2023-09-21 18:57:22 +1000

    Added comments for adminAuthRegister and adminAuthLogin

--- src/auth.js
+++ src/auth.js
@@ -1,9 +1,12 @@
+//Register a user with an email, password, and names, then returns their authUserId value.
 function adminAuthRegister (email, password, nameFirst, nameLast) {
   return {
     authUserId: 1
   }
+
 }
 
+//Given a registered user's email and password returns their authUserId value.
 function adminAuthLogin (email, password) {
   return {
     authUserId: 1

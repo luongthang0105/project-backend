3904f780cacc86ea87f0350c33bca5bf42c1ea40
Nuha <z5456988@ad.unsw.edu.au>
2023-09-21 13:47:46 +1000

    Added code to function stub adminUserDetails

---
+++ src/auth.js
@@ -0,0 +1,15 @@
+// This function is responsible for returning user details when the ID is given
+
+function adminUserDetails (authUserId) {
+    return {
+     user:
+        {
+          userId: 1,
+          name: 'Hayden Smith',
+          email: 'hayden.smith@unsw.edu.au',
+          numSuccessfulLogins: 3,
+          numFailedPasswordsSinceLastLogin: 1,
+        }
+      }
+}
+

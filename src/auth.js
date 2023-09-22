9dbdfbfecc0d561c8a23810960cb91afdb59e45e
Mutsuki <z5400771@ad.unsw.edu.au>
2023-09-22 16:57:42 +1000

    Added adminUserDetails stub

--- src/auth.js
+++ src/auth.js
@@ -13,3 +13,15 @@ function adminAuthLogin (email, password) {
   }
 }
 
+//Given an admin user's authUserId, return details about the user.
+function adminUserDetails (authUserId) {
+  return {
+    user: {
+      userId: 1,
+      name: 'Hayden Smith',
+      email: 'hayden.smith@unsw.edu.au',
+      numSuccessfulLogins: 3,
+      numFailedPasswordsSinceLastLogin: 1,
+    }
+  }
+}

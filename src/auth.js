ef3cbb6bd7b15145e77708f5eb73f43e7bba7a1f
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2023-09-26 14:38:09 +1000

    fix spacing auth.js

--- src/auth.js
+++ src/auth.js
@@ -1,9 +1,5 @@
 //Register a user with an email, password, and names, then returns their authUserId value.
 function adminAuthRegister(email, password, nameFirst, nameLast) {
-  return {
-    authUserId: 1
-  }
-
 }
 
 //Given a registered user's email and password returns their authUserId value.

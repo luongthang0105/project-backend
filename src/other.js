9054be0530888050e883ce721686925a7b58f02d
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2023-09-27 11:42:47 +1000

    implemented clear function to reset stateby calling setData

--- src/other.js
+++ src/other.js
@@ -1,4 +1,12 @@
+import { setData } from "./dataStore"  
+
 // This function resets the state of the application back to the start.
 function clear() {
+  setData({
+    users: [],
+    quizzes: [],
+    nextUserId: 0
+  })
   return {}
 }
+

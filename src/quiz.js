ad447de6ec7a1568303a6fc1eb19bfec2a74de64
Han <hanhhan2705@gmail.com>
2023-09-28 19:04:52 +1000

    implement adminQuizCreate with all edge cases

--- src/quiz.js
+++ src/quiz.js
@@ -16,12 +16,77 @@ function adminQuizDescriptionUpdate(authUserId, quizId, description) {
   return {}
 } 
 
+// Helper function to check if a string contains any number
+function hasNumbers(str) {
+  return /\d/.test(str);
+}
+
+// Helper function to check if a string contains any space
+function hasWhitespace(str) {
+  return /\s/.test(str);
+}
+
+// Helper function to get the current timestamp
+function getCurrentTimestamp () {
+  return Date.now()
+}
+
 // This function is responsible for creating a new quiz for a logged in user, given basic details about a new quiz
 function adminQuizCreate(authUserId, name, description) {
-  return {
-    quizId: 2
+  // AuthUserId is not a valid user
+  const currData = getData()
+  console.log(currData)
+  const id = authUserId
+  const validUserId = currData.users.find(({ authUserId }) => authUserId === id)
+  if (!validUserId) {
+    return { error: "AuthUserId is not a valid user" }
   }
+  
+  // Name contains invalid characters: alphanumeric
+  if (hasNumbers(name)) {
+    return { error: "AuthUserId is not a valid user" }
+  }
+
+  // Name contains invalid characters: spaces
+  if (hasWhitespace(name)) {
+    return { error: "AuthUserId is not a valid user" }
+  }
+  // Name is less than 3 characters long
+  if (name.length < 3) {
+    return { error: "Name is less than 3 characters long" }
+  }
+
+  // Name is more than 30 characters long
+  if (name.length > 30) {
+    return { error: "Name is more than 30 characters long" }
+  }
+
+  // Name is already used by the current logged in user for another quiz
+  for (const quiz of currData.quizzes) {
+    if (quiz.name === name) {
+      return { error: "Name is already used by the current logged in user for another quiz" }
+    }
+  }
+
+  // Description is more than 100 characters in length
+  if (description.length > 100) {
+    return { error: "Description is more than 100 characters in length" }
+  }
+  const timestamp = getCurrentTimestamp()
+  const newQuiz = {
+    quizId: currData.nextQuizId,
+    quizAuthorId: authUserId,
+    name: name,
+    timeCreated: timestamp,
+    timeLastEdited: timestamp,
+    description: description
+  }
+  currData.nextQuizId++
+  currData.quizzes.push(newQuiz)
+  console.log(newQuiz);
+  return { quizId: newQuiz.quizId }
 }
+
 
 // This function is responsible for permanently removing a particular quiz.
 function adminQuizRemove(authUserId, quizId) {

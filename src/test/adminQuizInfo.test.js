9e5cb5b79d5d7be083cd0ca9ad203bfb2fde14a2
Nuha <z5456988@ad.unsw.edu.au>
2023-10-02 10:37:37 +1100

    added quizcreate function usage, edited the authUserID tests and added an extra one

--- src/test/adminQuizInfo.test.js
+++ src/test/adminQuizInfo.test.js
@@ -5,20 +5,42 @@ import { adminAuthRegister } from "../auth"
 
 describe ('adminQuizInfo', () =>  {
   test ('AuthUserID is not valid', () => {
-    const result = adminQuizInfo('invalidUserId%', 1)
+    const user01 = adminAuthRegister('han@gmai.com', '2705uwuwuwu', 'Han', 'Hanh')
+    const result = adminQuizInfo(1, 1)
     expect(result).toStrictEqual({
       error: 'AuthUserID is not a valid user',
     })
-      })
+  })
+
+  test('User already exists', () => {
+    const user01 = adminAuthRegister('nuha@ou.com', '2705uwuwuwu', 'Ennc', 'Nuha')
+    const user02 = adminAuthRegister('nuha@ou.com', '2705uwuwuwu', 'Ennc', 'Nuha')
+    const result = adminQuizInfo(1, 1)
+    expect(result).toStrictEqual ({
+      error: 'AuthUserID is not a valid user'
+    })
+  })
+
+  test('New Quiz', () => {
+    const user03 = adminAuthRegister('nuha@ou.com', '2705uwuwuwu', 'Ennc', 'Nuha')
+    const AuthUserID = user03.authUserID
+    const quizName = 'ThisIsTheName'
+    const description = 'This is the description for Quiz 1'
+    const result02 = adminQuizCreate(authUserID, quizName, description)
+    expect(quizCreationResult).toStrictEqual(1);
+    expect(result).toBeGreaterThan(0)
+    expect(result).toHaveProperty('name', quizName);
+    expect(result).toHaveProperty('description', description);
+  })
   test ('QuizID is not owned by this user', () => {
-    const result2 = adminQuizInfo('userId', 999)
-    expect(result2).toStrictEqual({
+    const result03 = adminQuizInfo(1, 999)
+    expect(result03).toStrictEqual({
       error: 'Quiz ID does not refer to a quiz that this user owns',
-        });
-      })
+    })
+  })
   test ('QuizId is invalid', () => {
-    const result3 = adminQuizInfo('userID', 'invalidQuizID')
-    expect(result3).toStrictEqual({
+    const result04 = adminQuizInfo(1, -1)
+    expect(resul04).toStrictEqual({
       error: "QuizID is invalid"
     })
   })

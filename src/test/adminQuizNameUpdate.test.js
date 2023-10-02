672259f5e572f8d92b8ba167f0f49ceff795a406
Ryan Huynh <z5417046@ad.unsw.edu.au>
2023-10-02 16:13:07 +1100

    Added test for Quiz name update function

---
+++ src/test/adminQuizNameUpdate.test.js
@@ -0,0 +1,51 @@
+import {adminQuizNameUpdate} from "../quiz"
+import {clear} from "../other"
+
+beforeEach(() => {
+  clear()
+})
+
+describe('adminQuizNameUpdate', () => {
+  test('ERROR: AuthUserId is not a valid user', () => {
+    expect(adminQuizNameUpdate(1, 1, 'name')).toStrictEqual({
+      error: 'AuthUserId is not a valid user'
+    })
+  })
+
+  test('ERROR: Quiz ID does not refer to a valid quiz', () => {
+    expect(adminQuizNameUpdate(1, 1, 'name')).toStrictEqual({
+      error: 'Quiz ID does not refer to a valid quiz'
+    })
+  })
+
+  test('ERROR: Quiz ID does not refer to a quiz that this user owns', () => {
+    expect(adminQuizNameUpdate(1, 1, 'name')).toStrictEqual({
+      error: 'Quiz ID does not refer to a quiz that this user owns'
+    })
+  })
+
+  test('ERROR: Name contains invalid characters', () => {
+    expect(adminQuizNameUpdate(1, 1, 'name')).toStrictEqual({
+      error: 'Name contains invalid characters'
+    })
+  })
+
+  test('ERROR: Name is less than 3 characters long', () => {
+    expect(adminQuizNameUpdate(1, 1, 'name')).toStrictEqual({
+      error: 'Name is less than 3 characters long'
+    })
+  })
+
+  test('ERROR: Name is greater than 30 characters long', () => {
+    expect(adminQuizNameUpdate(1, 1, 'name')).toStrictEqual({
+      error: 'Name is greater than 30 characters long'
+    })
+  })
+
+  test('ERROR: Name is already used for another quiz', () => {
+    expect(adminQuizNameUpdate(1, 1, 'name')).toStrictEqual({
+      error: 'Name is already used for another quiz'
+    })
+  })
+
+})

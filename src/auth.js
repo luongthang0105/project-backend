b86eb178735341fb49b769c1a422c62e38be91fc
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2023-10-05 17:04:14 +1100

    remove commented code

--- src/auth.js
+++ src/auth.js
@@ -74,23 +74,6 @@ function adminAuthRegister(email, password, nameFirst, nameLast) {
 
   return { authUserId: user.authUserId }
 }
-  
-// This function is responsible for returning user details when the ID is given
-
-/*
-function adminUserDetails (authUserId) {
-  return {
-    user:
-      {
-        userId: 1,
-        name: 'Hayden Smith',
-        email: 'hayden.smith@unsw.edu.au',
-        numSuccessfulLogins: 3,
-        numFailedPasswordsSinceLastLogin: 1,
-      }
-    }
-}
-*/
 
 //Given a registered user's email and password returns their authUserId value.
 function adminAuthLogin(email, password) {

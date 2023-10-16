ebc1e20e6668234d576d736a7b65c3ae8d0bed41
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2023-10-16 17:33:22 +1100

    migrate in memory storage to data persistence

---
+++ src/dataStore.ts
@@ -0,0 +1,37 @@
+// YOU SHOULD MODIFY THIS OBJECT BELOW
+import fs from 'fs'
+
+let data = {
+  users: [],
+  quizzes: [],
+  nextUserId: 0,
+  nextQuizId: 0
+}
+
+// YOU SHOULDNT NEED TO MODIFY THE FUNCTIONS BELOW IN ITERATION 1
+
+/*
+Example usage
+    let store = getData()
+    console.log(store) # Prints { 'names': ['Hayden', 'Tam', 'Rani', 'Giuliana', 'Rando'] }
+
+    names = store.names
+
+    names.pop()
+    names.push('Jake')
+
+    console.log(store) # Prints { 'names': ['Hayden', 'Tam', 'Rani', 'Giuliana', 'Jake'] }
+    setData(store)
+*/
+
+// Use get() to access the data
+function getData() {
+  return JSON.parse(String(fs.readFileSync('./dataStore.json')))
+}
+
+// Use set(newData) to pass in the entire data object, with modifications made
+function setData(newData) {
+  fs.writeFileSync('./dataStore.json', JSON.stringify(newData))
+}
+
+export { getData, setData }

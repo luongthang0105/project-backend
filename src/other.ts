b2491abd305792dc4ead12c050b4116a774f46e9
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2023-10-18 17:03:27 +1100

    remove .js extension for imported file

--- src/other.ts
+++ src/other.ts
@@ -1,10 +1,11 @@
-import { setData } from "./dataStore.js"  
+import { setData } from "./dataStore"  
 
 // This function resets the state of the application back to the start.
 function clear() {
   setData({
     users: [],
     quizzes: [],
+    trash: [],
     nextUserId: 0,
     nextQuizId: 0
   })

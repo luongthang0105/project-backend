c8d42d931062eb9c33edffdafb768438fbcf2028
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2023-10-18 19:00:34 +1100

    change type name of dataStore to PascalCase

--- src/dataStore.ts
+++ src/dataStore.ts
@@ -1,6 +1,6 @@
 // YOU SHOULD MODIFY THIS OBJECT BELOW
 import fs from 'fs'
-import { dataStore } from "./types"
+import { DataStore } from "./types"
 
 // YOU SHOULDNT NEED TO MODIFY THE FUNCTIONS BELOW IN ITERATION 1
 
@@ -20,10 +20,10 @@ Example usage
 */
 
 // Use get() to access the data
-const getData = (): dataStore => JSON.parse(String(fs.readFileSync('./data.json')))
+const getData = (): DataStore => JSON.parse(String(fs.readFileSync('./data.json')))
 
 // Use set(newData) to pass in the entire data object, with modifications made
-function setData(newData: dataStore) {
+function setData(newData: DataStore) {
   fs.writeFileSync('./data.json', JSON.stringify(newData))
 }
 

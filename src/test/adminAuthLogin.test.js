7feb02b67df8e39b7698c06170fc3b8965200add
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2023-10-21 18:10:34 +1100

    rewrite tests for adminAuthLogin

--- src/test/adminAuthLogin.test.js
+++ src/test/adminAuthLogin.test.js
@@ -1,51 +1,47 @@
-import { adminAuthRegister, adminAuthLogin } from "../auth"
+import { adminAuthRegister, adminAuthLogin } from "../testWrappers"
 import { clear } from "../other"
 describe("adminAuthLogin", () => {
   beforeEach(() => {
     clear()
+    let user = adminAuthRegister("javascript@gmail.com", "aikfnrg7", "Java", "Script")
+    expect(user.statusCode).toEqual(200)
   })
   test("ERROR: Email address does not exist", () => {
-    adminAuthRegister("javascript@gmail.com", "aikfnrg7", "Java", "Script")
     const error = adminAuthLogin("python@gmail.com", "aikfnrg7")
-    expect(error).toEqual({ error: "Email adress does not exist" })
+    expect(error).toStrictEqual({statusCode: 400, error: "Email adress does not exist" })
   })
   test("ERROR: Password is not correct for the given email", () => {
-    adminAuthRegister("javascript@gmail.com", "aikfnrg7", "Java", "Script")
     const error = adminAuthLogin("javascript@gmail.com", "aikfnrg8")
-    expect(error).toEqual({
+    expect(error).toStrictEqual({
       error: "Password is not correct for the given email",
+      statusCode: 400
     })
   })
   test("ERROR: Password is case-insensitive", () => {
-    adminAuthRegister("javascript@gmail.com", "aikfnrg7", "Java", "Script")
     const error = adminAuthLogin("javascript@gmail.com", "Aikfnrg7")
-    expect(error).toEqual({
+    expect(error).toStrictEqual({
       error: "Password is not correct for the given email",
+      statusCode: 400
     })
   })
-  test("ERROR: No registeration done", () => {
+  test("ERROR: No registration done", () => {
     const error = adminAuthLogin("java@gmail.com", "gdnkgeg4")
-    expect(error).toEqual({ error: "Email adress does not exist" })
+    expect(error).toStrictEqual({ statusCode: 400, error: "Email adress does not exist" })
   })
   test("Success: 1 person registered", () => {
-    const userId1 = adminAuthRegister(
-      "javascript@gmail.com",
-      "aikfnrg7",
-      "Java",
-      "Script"
-    )
     const success = adminAuthLogin("javascript@gmail.com", "aikfnrg7")
-    expect(success).toEqual(userId1)
+    expect(success).toStrictEqual({statusCode: 200, token: expect.any(String)})
   })
   test("Success: More than 1 person registered", () => {
-    adminAuthRegister("javascript@gmail.com", "aikfnrg7", "Java", "Script")
-    const userId2 = adminAuthRegister(
+    const user2 = adminAuthRegister(
       "java@gmail.com",
       "gdnkgeg4",
       "Hello",
       "World"
     )
+    expect(user2).toStrictEqual({ statusCode: 200, token: expect.any(String)})
+    
     const success = adminAuthLogin("java@gmail.com", "gdnkgeg4")
-    expect(success).toEqual(userId2)
+    expect(success).toStrictEqual({ statusCode: 200, token: expect.any(String)})
   })
 })

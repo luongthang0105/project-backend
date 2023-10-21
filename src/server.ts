f64ad714b62a3924bbd4f751df1f6bb179079a91
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2023-10-21 18:43:12 +1100

    move http apis above the middleware

--- src/server.ts
+++ src/server.ts
@@ -60,24 +60,6 @@ app.post("/v1/admin/auth/register", (req: Request, res: Response) => {
 
   res.json(result)
 })
-// ====================================================================
-//  ================= WORK IS DONE ABOVE THIS LINE ===================
-// ====================================================================
-
-app.use((req: Request, res: Response) => {
-  const error = `
-    404 Not found - This could be because:
-      0. You have defined routes below (not above) this middleware in server.ts
-      1. You have not implemented the route ${req.method} ${req.path}
-      2. There is a typo in either your test or server, e.g. /posts/list in one
-         and, incorrectly, /post/list in the other
-      3. You are using ts-node (instead of ts-node-dev) to start your server and
-         have forgotten to manually restart to load the new changes
-      4. You've forgotten a leading slash (/), e.g. you have posts/list instead
-         of /posts/list in your server.ts or test file
-  `
-  res.status(404).json({ error })
-})
 
 app.put(
   "/v1/admin/quiz/{quizid}/description",
@@ -115,6 +97,26 @@ app.post("/v1/admin/quiz", (req: Request, res: Response) => {
 
   res.json(result);
 });
+
+// ====================================================================
+//  ================= WORK IS DONE ABOVE THIS LINE ===================
+// ====================================================================
+
+app.use((req: Request, res: Response) => {
+  const error = `
+    404 Not found - This could be because:
+      0. You have defined routes below (not above) this middleware in server.ts
+      1. You have not implemented the route ${req.method} ${req.path}
+      2. There is a typo in either your test or server, e.g. /posts/list in one
+         and, incorrectly, /post/list in the other
+      3. You are using ts-node (instead of ts-node-dev) to start your server and
+         have forgotten to manually restart to load the new changes
+      4. You've forgotten a leading slash (/), e.g. you have posts/list instead
+         of /posts/list in your server.ts or test file
+  `
+  res.status(404).json({ error })
+})
+
 // start server
 const server = app.listen(PORT, HOST, () => {
   // DO NOT CHANGE THIS LINE

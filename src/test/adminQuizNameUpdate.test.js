3538e9ff9465f65cc21137d42d9caa9c788d7879
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2023-10-22 18:38:06 +1100

    adjust quiznameudpate tests to adapt new format from test wrappers

--- src/test/adminQuizNameUpdate.test.js
+++ src/test/adminQuizNameUpdate.test.js
@@ -13,6 +13,7 @@ describe("adminQuizNameUpdate", () => {
   const invalidToken = {
     token: "-1"
   }
+
   test("ERROR: AuthUserId is not a valid user", () => {
     expect(adminQuizNameUpdate(invalidToken, -1, "name")).toStrictEqual({
       content: {error: "Token is empty or invalid (does not refer to valid logged in user session)"},
@@ -39,20 +40,20 @@ describe("adminQuizNameUpdate", () => {
       "2705uwuwuwuwuwuw",
       "Han",
       "Hanh"
-    )
+    ).content
     const user02 = adminAuthRegister(
       "hanh@gmai.com",
       "270555uwuwuwuwuwuw",
       "Hanh",
       "Haanh"
-    )
+    ).content
     const quiz01 = adminQuizCreate(
       user01.authUserId,
       "quiz",
       "This is my quiz"
-    )
+    ).content
     expect(
-      adminQuizNameUpdate(user02.authUserId, quiz01.quizId, "name")
+      adminQuizNameUpdate(user02, quiz01.quizId, "name")
     ).toStrictEqual({
       content: {error: "Valid token is provided, but user is not an owner of this quiz"},
       statusCode: 403
@@ -72,14 +73,14 @@ describe("adminQuizNameUpdate", () => {
       "2705uwuwuwuwuwuw",
       "Han",
       "Hanh"
-    )
+    ).content
     const quiz01 = adminQuizCreate(
       user01.authUserId,
       "quiz",
       "This is my quiz"
-    )
+    ).content
     expect(
-      adminQuizNameUpdate(user01.authUserId, quiz01.quizId, name)
+      adminQuizNameUpdate(user01, quiz01.quizId, name)
     ).toStrictEqual({
       content: {error: "Name contains invalid characters"},
       statusCode: 400
@@ -92,14 +93,14 @@ describe("adminQuizNameUpdate", () => {
       "2705uwuwuwuwuwuw",
       "Han",
       "Hanh"
-    )
+    ).content
     const quiz01 = adminQuizCreate(
       user01.authUserId,
       "quiz",
       "This is my quiz"
-    )
+    ).content
     expect(
-      adminQuizNameUpdate(user01.authUserId, quiz01.quizId, "na")
+      adminQuizNameUpdate(user01, quiz01.quizId, "na")
     ).toStrictEqual({
       content: {error: "Name is less than 3 characters long"},
       statusCode: 400
@@ -112,12 +113,12 @@ describe("adminQuizNameUpdate", () => {
       "2705uwuwuwuwuwuw",
       "Han",
       "Hanh"
-    )
+    ).content
     const quiz01 = adminQuizCreate(
       user01.authUserId,
       "quiz",
       "This is my quiz"
-    )
+    ).content
     expect(
       adminQuizNameUpdate(
         user01.authUserId,
@@ -136,19 +137,19 @@ describe("adminQuizNameUpdate", () => {
       "2705uwuwuwuwuwuw",
       "Han",
       "Hanh"
-    )
+    ).content
     const quiz01 = adminQuizCreate(
       user01.authUserId,
       "quiz01",
       "This is my quiz"
-    )
+    ).content
     const quiz02 = adminQuizCreate(
       user01.authUserId,
       "quiz02",
       "This is my quiz"
-    )
+    ).content
     expect(
-      adminQuizNameUpdate(user01.authUserId, quiz01.quizId, "quiz02")
+      adminQuizNameUpdate(user01, quiz01.quizId, "quiz02")
     ).toStrictEqual({
       error:
         "Name is already used by the current logged in user for another quiz",
@@ -157,12 +158,12 @@ describe("adminQuizNameUpdate", () => {
   })
 
   test('Success: Returns {} if no error, 1 user', () => {
-    const user01 = adminAuthRegister('han@gmai.com', '2705uwuwuwuwuwuw', 'Han', 'Hanh')
-    const quiz01 = adminQuizCreate(user01.authUserId, 'quiz01', 'This is my quiz')
+    const user01 = adminAuthRegister('han@gmai.com', '2705uwuwuwuwuwuw', 'Han', 'Hanh').content
+    const quiz01 = adminQuizCreate(user01, 'quiz01', 'This is my quiz').content
 
-    expect(adminQuizNameUpdate(user01.authUserId, quiz01.quizId, 'name')).toStrictEqual({})
+    expect(adminQuizNameUpdate(user01, quiz01.quizId, 'name').content).toStrictEqual({})
 
-    const quizInfo = adminQuizInfo(user01.authUserId, quiz01.quizId)
+    const quizInfo = adminQuizInfo(user01, quiz01.quizId).content
     expect(quizInfo.name).toStrictEqual("name")
     expect(quizInfo.timeLastEdited).toBeGreaterThanOrEqual(
       quizInfo.timeCreated
@@ -170,26 +171,26 @@ describe("adminQuizNameUpdate", () => {
   })
 
   test('Success: Returns {} if no error, 2 different users', () => {
-    const user01 = adminAuthRegister('han@gmai.com', '2705uwuwuwuwuwuw', 'Han', 'Hanh')
-    const quiz01 = adminQuizCreate(user01.authUserId, 'quiz01', 'This is my quiz')
+    const user01 = adminAuthRegister('han@gmai.com', '2705uwuwuwuwuwuw', 'Han', 'Hanh').content
+    const quiz01 = adminQuizCreate(user01.authUserId, 'quiz01', 'This is my quiz').content
     
-    const user02 = adminAuthRegister('han222@gmai.com', '2705uwuwuwuwuwuw', 'Han', 'Hanh')
-    const quiz02 = adminQuizCreate(user02.authUserId, 'quiz02', 'This is my quiz 2')
+    const user02 = adminAuthRegister('han222@gmai.com', '2705uwuwuwuwuwuw', 'Han', 'Hanh').content
+    const quiz02 = adminQuizCreate(user02.authUserId, 'quiz02', 'This is my quiz 2').content
 
-    expect(adminQuizNameUpdate(user02.authUserId, quiz02.quizId, 'name')).toStrictEqual({})
+    expect(adminQuizNameUpdate(user02, quiz02.quizId, 'name').content).toStrictEqual({})
 
-    const quizInfo = adminQuizInfo(user02.authUserId, quiz02.quizId)
+    const quizInfo = adminQuizInfo(user02, quiz02.quizId).content
     expect(quizInfo.name).toStrictEqual("name")
     expect(quizInfo.timeLastEdited).toBeGreaterThanOrEqual(quizInfo.timeCreated)
   })
 
   test('Success: Returns {} if no error, updating same name', () => {
-    const user01 = adminAuthRegister('han@gmai.com', '2705uwuwuwuwuwuw', 'Han', 'Hanh')
-    const quiz01 = adminQuizCreate(user01.authUserId, 'quiz01', 'This is my quiz')    
+    const user01 = adminAuthRegister('han@gmai.com', '2705uwuwuwuwuwuw', 'Han', 'Hanh').content
+    const quiz01 = adminQuizCreate(user01.authUserId, 'quiz01', 'This is my quiz').content    
     expect(
-      adminQuizNameUpdate(user01.authUserId, quiz01.quizId, "quiz01")
+      adminQuizNameUpdate(user01.authUserId, quiz01.quizId, "quiz01").content
     ).toStrictEqual({})
-    const quizInfo = adminQuizInfo(user01.authUserId, quiz01.quizId)
+    const quizInfo = adminQuizInfo(user01, quiz01.quizId).content
     expect(quizInfo.name).toStrictEqual("quiz01")
     expect(quizInfo.timeLastEdited).toBeGreaterThanOrEqual(
       quizInfo.timeCreated

bbc96100e0f04fbc65ec4e5f235dc541616b66bb
Nuha <z5456988@ad.unsw.edu.au>
2023-10-24 11:26:01 +1100

    minor adjustments to code to match testwrappers

--- src/test/adminAuthLogout.test.js
+++ src/test/adminAuthLogout.test.js
@@ -1,18 +1,21 @@
 import { adminAuthRegister, adminAuthLogin } from "../testWrappers"
 import { clear } from "../other"
-let user = adminAuthRegister('nuha381@gmail,com','Fjeyvwr43', 'Enn', 'Cee').content
 
 describe ('adminAuthLogout', () => {
+  let user 
+  beforeEach(() => {
+    clear()
+      user = adminAuthRegister('nuha381@gmail,com','Fjeyvwr43', 'Enn', 'Cee').content
+  })
   it ('should successfully logout', () => {
     const token = user.token
     const result = adminAuthLogout(token)
-    expect(clear).toHaveBeenCalled()
-    expect(result.statusCode).toBe(200)
+    expect(result).toStrictEqual({})
     })
   it ('returns error when token is empty or invalid', () => {
     const invalidToken = ''
     const result = adminAuthLogout(invalidToken)
     expect(result.statusCode).toBe(401)
-    expect(result.content).toEqual({ error: "error" });
+    expect(result.error).toEqual("error");
   })
 })

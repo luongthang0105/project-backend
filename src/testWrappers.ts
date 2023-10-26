2c545f86d9ef2197dac0df5be7c655ec654908d7
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2023-10-27 03:05:16 +1100

    write test wrapper for adminQuizTransfer

--- src/testWrappers.ts
+++ src/testWrappers.ts
@@ -306,3 +306,23 @@ export const adminQuizDeleteQuestion = (
     statusCode: res.statusCode
   };
 };
+
+export const adminQuizTransfer = (
+  quizId: number,
+  tokenObject: ReturnedToken,
+  userEmail: string
+): {content: EmptyObject | ErrorObject, statusCode: number} => {
+  const route = '/v1/admin/quiz/' + quizId + '/transfer';
+
+  const res = request('POST', SERVER_URL + route, {
+    json: {
+      token: tokenObject.token,
+      userEmail: userEmail
+    }
+  });
+
+  return {
+    content: JSON.parse(res.body.toString()),
+    statusCode: res.statusCode
+  };
+};

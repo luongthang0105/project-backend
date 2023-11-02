ad4d85f4c17769a2e1518329be989a637d023928
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2023-11-03 10:45:54 +1100

    write api for v2 user details

--- src/server.ts
+++ src/server.ts
@@ -327,6 +327,12 @@ app.post('/v2/admin/auth/logout', (req: Request, res: Response) => {
   res.json(result);
 });
 
+// adminUserDetails V2
+app.get('/v2/admin/user/details', (req: Request, res: Response) => {
+  const token = req.headers.token as string;
+  res.json(adminUserDetails(token));
+});
+
 // ====================================================================
 //  ================= WORK IS DONE ABOVE THIS LINE ===================
 // ====================================================================

a5ec0abd5823eb6414b8be3a918684611daabd74
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2023-11-03 10:41:56 +1100

    add test wrapper for v2 userdetails

--- src/testWrappersV2.ts
+++ src/testWrappersV2.ts
@@ -3,6 +3,7 @@ import { port, url } from './config.json';
 import {
   EmptyObject,
   ReturnedToken,
+  UserDetails,
 } from './types';
 
 const SERVER_URL = `${url}:${port}`;
@@ -25,6 +26,29 @@ export const adminAuthLogout = (
 
   const res = request('POST', SERVER_URL + route, {
     headers: {
+      token: tokenObject.token,
+    },
+  });
+
+  return {
+    content: JSON.parse(res.body.toString()),
+    statusCode: res.statusCode,
+  };
+};
+
+/**
+ * Retrieves user details by sending a GET request to the server's user details endpoint.
+ *
+ * @param tokenObject - An object containing the authentication token for user details retrieval.
+ * @param tokenObject.token - The authentication token for the request.
+ *
+ * @returns An object containing the response content (UserDetails or ErrorObject) and the HTTP status code of the user details request.
+ */
+export const adminUserDetails = (tokenObject: {
+  token: string;
+}): { content: UserDetails; statusCode: number } => {
+  const res = request('GET', SERVER_URL + '/v2/admin/user/details', {
+    qs: {
       token: tokenObject.token,
     },
   });

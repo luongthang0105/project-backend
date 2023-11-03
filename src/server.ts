c2e9650cbc4ecc79f4a154d70f970f16ede399da
Mutsuki <z5400771@ad.unsw.edu.au>
2023-11-03 15:35:25 +1100

    Update server.ts by adding V2 of adminQuizTrashEmpty route

--- src/server.ts
+++ src/server.ts
@@ -327,6 +327,16 @@ app.post('/v2/admin/auth/logout', (req: Request, res: Response) => {
   res.json(result);
 });
 
+// adminQuizTrashEmpty
+app.delete('/v2/admin/quiz/trash/empty', (req: Request, res: Response) => {
+  const quizIds: number[] = JSON.parse(req.query.quizIds as string);
+
+  const token = req.headers.token as string;
+  const result = adminQuizTrashEmpty(token, quizIds);
+
+  res.json(result);
+});
+
 // ====================================================================
 //  ================= WORK IS DONE ABOVE THIS LINE ===================
 // ====================================================================

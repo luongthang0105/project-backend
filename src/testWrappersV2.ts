6030cbc7f234c68f51cad22c28c2e92d43d7f265
Mutsuki <z5400771@ad.unsw.edu.au>
2023-11-03 20:06:52 +1100

    Fix lint errors in server.ts

--- src/testWrappersV2.ts
+++ src/testWrappersV2.ts
@@ -3,6 +3,7 @@ import { port, url } from './config.json';
 import {
   EmptyObject,
   ReturnedToken,
+  QuizList
 } from './types';
 
 const SERVER_URL = `${url}:${port}`;
@@ -61,5 +62,85 @@ export const adminQuizTrashEmpty = (
   return {
     content: JSON.parse(res.body.toString()),
     statusCode: res.statusCode
+  };
+};
+
+/**
+ * Restores a quiz by sending a POST request to the server's quiz restoration endpoint.
+ *
+ * @param tokenObject - An object containing the authentication token for quiz restoration.
+ * @param tokenObject.token - The authentication token for the request.
+ * @param quizId - The unique identifier of the quiz to be restored.
+ *
+ * @returns An object containing the response content (EmptyObject or ErrorObject) and the HTTP status code of the quiz restoration request.
+ */
+export const adminQuizRestore = (
+  tokenObject: ReturnedToken,
+  quizId: number
+): { content: EmptyObject; statusCode: number } => {
+  const res = request('POST', SERVER_URL + '/v2/admin/quiz/' + quizId + '/restore', {
+    headers: {
+      token: tokenObject.token
+    },
+  });
+
+  return {
+    content: JSON.parse(res.body.toString()),
+    statusCode: res.statusCode,
+  };
+};
+
+/**
+ * Views the list of quizzes in the "trash" by sending a GET request to the server's trash quiz list endpoint.
+ *
+ * @param tokenObject - An object containing the authentication token for viewing the trash quiz list.
+ * @param tokenObject.token - The authentication token for the request.
+ *
+ * @returns An object containing the response content (QuizList or ErrorObject) and the HTTP status code of the trash quiz list request.
+ */
+export const adminQuizViewTrash = (tokenObject: {
+  token: string;
+}): { content: QuizList; statusCode: number } => {
+  const res = request('GET', SERVER_URL + '/v2/admin/quiz/trash', {
+    headers: {
+      token: tokenObject.token,
+    },
+  });
+
+  return {
+    content: JSON.parse(res.body.toString()),
+    statusCode: res.statusCode,
+  };
+};
+
+/**
+ * Updates the description of a quiz by sending a PUT request to the server's quiz description update endpoint.
+ *
+ * @param tokenObject - An object containing the authentication token for quiz description update.
+ * @param tokenObject.token - The authentication token for the request.
+ * @param quizId - The unique identifier of the quiz for which the description is to be updated.
+ * @param description - The new description for the quiz.
+ *
+ * @returns An object containing the response content (EmptyObject or ErrorObject) and the HTTP status code of the quiz description update request.
+ */
+export const adminQuizDescriptionUpdate = (
+  tokenObject: ReturnedToken,
+  quizId: number,
+  description: string
+): { content: EmptyObject; statusCode: number } => {
+  const route = '/v2/admin/quiz/' + quizId + '/description';
+
+  const res = request('PUT', SERVER_URL + route, {
+    headers: {
+      token: tokenObject.token,
+    },
+    json: {
+      description: description,
+    },
+  });
+
+  return {
+    content: JSON.parse(res.body.toString()),
+    statusCode: res.statusCode,
   };
 };

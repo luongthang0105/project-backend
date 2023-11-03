cd2fdb18748de7d43e198565e0cd7614b3df6c82
Mutsuki <z5400771@ad.unsw.edu.au>
2023-11-03 15:33:59 +1100

    Update server.ts by adding V2 of adminQuizTrashEmpty route

--- src/testWrappersV2.ts
+++ src/testWrappersV2.ts
@@ -34,3 +34,32 @@ export const adminAuthLogout = (
     statusCode: res.statusCode,
   };
 };
+
+/**
+ * Empties the trash (permanently deletes quizzes) by sending a DELETE request to the server's trash emptying endpoint.
+ *
+ * @param tokenObject - An object containing the authentication token for emptying the trash.
+ * @param tokenObject.token - The authentication token for the request.
+ * @param quizIds - A string containing the IDs of quizzes to be permanently deleted, separated by commas.
+ *
+ * @returns An object containing the response content (EmptyObject or ErrorObject) and the HTTP status code of the trash emptying request.
+ */
+export const adminQuizTrashEmpty = (
+  tokenObject: ReturnedToken,
+  quizIds: string
+): {content: EmptyObject, statusCode: number} => {
+  const route = '/v2/admin/quiz/trash/empty';
+  const res = request('DELETE', SERVER_URL + route, {
+    headers: {
+      token: tokenObject.token,
+    },
+    qs: {
+      quizIds: quizIds
+    }
+  });
+
+  return {
+    content: JSON.parse(res.body.toString()),
+    statusCode: res.statusCode
+  };
+};

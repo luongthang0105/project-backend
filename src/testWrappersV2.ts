b6a4ccd54aa5a5d31a6cf491c20d42b96c4a6f5c
Mutsuki <z5400771@ad.unsw.edu.au>
2023-11-03 13:46:22 +1100

    Update server.ts for V2 of adminQuizDescriptionUpdate

--- src/testWrappersV2.ts
+++ src/testWrappersV2.ts
@@ -34,3 +34,35 @@ export const adminAuthLogout = (
     statusCode: res.statusCode,
   };
 };
+
+/**
+ * Updates the description of a quiz by sending a PUT request to the server's quiz description update endpoint.
+ *
+ * @param tokenObject - An object containing the authentication token for quiz description update.
+ * @param tokenObject.token - The authentication token for the request.
+ * @param quizId - The unique identifier of the quiz for which the description is to be updated.
+ * @param description - The new description for the quiz.
+ *
+ * @returns An object containing the response content (EmptyObject or ErrorObject) and the HTTP status code of the quiz description update request.
+ */
+export const adminQuizDescriptionUpdate = (
+  tokenObject: ReturnedToken,
+  quizId: number,
+  description: string
+): { content: EmptyObject; statusCode: number } => {
+  const route = '/v2/admin/quiz/' + quizId + '/description';
+
+  const res = request('PUT', SERVER_URL + route, {
+    headers: {
+      token: tokenObject.token,
+    },
+    json: {
+      description: description,
+    },
+  });
+
+  return {
+    content: JSON.parse(res.body.toString()),
+    statusCode: res.statusCode,
+  };
+};

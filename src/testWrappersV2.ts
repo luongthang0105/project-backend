45af63f15c3cf643ec005ff21375f0740903cdd5
Ryan Huynh <z5417046@ad.unsw.edu.au>
2023-11-07 13:02:32 +1100

    Fixed merging in testWrappersV2.ts

--- src/testWrappersV2.ts
+++ src/testWrappersV2.ts
@@ -315,6 +315,7 @@ export const adminQuizCreateQuestion = (
 
 /**
 <<<<<<< HEAD
+<<<<<<< HEAD
  * Retrieves information about a quiz by sending a GET request to the server's quiz information endpoint.
  *
  * @param tokenObject - An object containing the authentication token for quiz information retrieval.

8d4e6950819089ddf34d84036b998cea5676453e
Ryan Huynh <z5417046@ad.unsw.edu.au>
2023-11-07 17:39:33 +1100

    Fixed merge conflicts in testWrappersV2.ts

--- src/testWrappersV2.ts
+++ src/testWrappersV2.ts
@@ -154,6 +154,7 @@ export const adminQuizDescriptionUpdate = (
 
 /**
 <<<<<<< HEAD
+<<<<<<< HEAD
  * Retrieves a list of quizzes by sending a GET request to the server's quiz list endpoint.
  *
  * @param tokenObject - An object containing the authentication token for quiz list retrieval.

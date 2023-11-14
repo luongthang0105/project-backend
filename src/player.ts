0260261ed2926bbfb57b02f936b654a3cebd3efb
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2023-11-14 18:09:43 +1100

    implement playerStatus function

--- src/player.ts
+++ src/player.ts
@@ -1,61 +1,102 @@
-import { getData, setData } from "./dataStore";
-import HTTPError from "http-errors";
-import { generateRandomName } from "./playerHelper";
-import { Player } from "./types";
-import { randomSessionId } from "./authHelper";
+import { getData, setData } from "./dataStore"
+import HTTPError from "http-errors"
+import { generateRandomName } from "./playerHelper"
+import { Player } from "./types"
+import { randomSessionId } from "./authHelper"
 
-export const playerJoinSession = (sessionId: number, name: string): { playerId: number } => {
-  const data = getData();
+export const playerJoinSession = (
+  sessionId: number,
+  name: string,
+): { playerId: number } => {
+  const data = getData()
 
-  const validSession = data.quizSessions.find( (quizSession) => quizSession.quizSessionId === sessionId);
-  
+  const validSession = data.quizSessions.find(
+    (quizSession) => quizSession.quizSessionId === sessionId,
+  )
+
   // Error: Session does not exist
   if (!validSession) {
-    throw HTTPError(400, "Session doesn't exist (this is undefined behaviour and won't be tested)")
+    throw HTTPError(
+      400,
+      "Session doesn't exist (this is undefined behaviour and won't be tested)",
+    )
   }
 
   // Error: Session is not in LOBBY state
-  if (validSession.state !== 'LOBBY') {
+  if (validSession.state !== "LOBBY") {
     throw HTTPError(400, "Session is not in LOBBY state")
-    
   }
 
-  
   // If name is empty, randomly generate it according to the structure [5 letters][3 numbers],
   // where there are no repetitions of numbers or characters within the same name
-  if (name === '') {
+  if (name === "") {
     // generate the random name and make sure it's unique among the others name in the session
     while (true) {
-      name = generateRandomName();
-      let playerWithSameName = validSession.players.find( (playerName) => playerName === name)
-      
-      if (!playerWithSameName) break;
+      name = generateRandomName()
+      let playerWithSameName = validSession.players.find(
+        (playerName) => playerName === name,
+      )
+
+      if (!playerWithSameName) break
     }
-  } 
-  
+  }
+
   // If name is not empty, check for this:
   // Error: Name of user entered is not unique (compared to other users who have already joined)
   else {
-    let playerWithSameName = validSession.players.find( (playerName) => playerName === name)
+    let playerWithSameName = validSession.players.find(
+      (playerName) => playerName === name,
+    )
     if (playerWithSameName) {
-      throw HTTPError(400, "Name of user entered is not unique (compared to other users who have already joined)")
+      throw HTTPError(
+        400,
+        "Name of user entered is not unique (compared to other users who have already joined)",
+      )
     }
   }
 
   const newPlayer: Player = {
     playerId: data.nextPlayerId,
     name: name,
-    sessionJoined: sessionId
+    sessionJoined: sessionId,
   }
 
-  data.nextPlayerId += 1;
+  data.nextPlayerId += 1
   // add new player to data
-  data.players.push(newPlayer);
+  data.players.push(newPlayer)
 
   // add players name to session players field
-  validSession.players.push(name);
+  validSession.players.push(name)
 
-  setData(data);
-  
-  return { playerId: newPlayer.playerId };
+  setData(data)
+
+  return { playerId: newPlayer.playerId }
+}
+
+const playerStatus = (
+  playerId: number,
+): {
+  state: string
+  numQuestions: number
+  atQuestion: number
+} => {
+  const data = getData()
+
+  // Error: Player ID does not exist
+  const validPlayer = data.players.find(
+    (player) => player.playerId === playerId,
+  )
+  if (!validPlayer) {
+    throw HTTPError(400, "Player ID does not exist")
+  }
+
+  const currSession = data.quizSessions.find(
+    (quizSession) => quizSession.quizSessionId === validPlayer.sessionJoined
+  )
+
+  return {
+    state: currSession.state,
+    atQuestion: currSession.atQuestion,
+    numQuestions: currSession.metadata.numQuestions
+  }
 }

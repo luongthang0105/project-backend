aef5bd4b7a4c300d327feca11b55cbb4d67cfcfa
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2023-11-14 19:23:40 +1100

    add test playerStatus checks for state at question 2

--- src/V1-tests/playerStatus.test.ts
+++ src/V1-tests/playerStatus.test.ts
@@ -59,6 +59,16 @@ describe('playerJoinSession', () => {
       questInfo1.thumbnailUrl,
     ).content.questionId;
 
+    question1 = adminQuizCreateQuestion(
+      user1,
+      quiz1.quizId,
+      questInfo1.question,
+      questInfo1.duration,
+      questInfo1.points,
+      questInfo1.answers,
+      questInfo1.thumbnailUrl,
+    ).content.questionId;
+
     session1 = adminQuizSessionStart(user1, quiz1.quizId, 3).content.sessionId;
   })
   
@@ -73,7 +83,7 @@ describe('playerJoinSession', () => {
 
     const status = playerStatus(player1.content.playerId + 1);
 
-    expect(playerStatus).toStrictEqual({
+    expect(status).toStrictEqual({
       content: {
         error: 'Player ID does not exist'
       },
@@ -92,17 +102,17 @@ describe('playerJoinSession', () => {
 
     const status = playerStatus(player1.content.playerId);
 
-    expect(playerStatus).toStrictEqual({
+    expect(status).toStrictEqual({
       content: {
         state: "LOBBY",
-        numQuestions: 1,
+        numQuestions: 2,
         atQuestion: 0
       },
       statusCode: 200
     })
   })
 
-  test('Success: At question 1', () => {
+  test('(NOT YET TO BE TESTED) Success: At question 1', () => {
     const player1 = playerJoinSession(session1, "Thomas");
     expect(player1).toStrictEqual({
       content: {
@@ -118,15 +128,15 @@ describe('playerJoinSession', () => {
 
     expect(playerStatus).toStrictEqual({
       content: {
-        state: "LOBBY",
-        numQuestions: 1,
+        state: "QUESTION_COUNTDOWN",
+        numQuestions: 2,
         atQuestion: 1
       },
       statusCode: 200
     })
   })
 
-  test('Success: At END state', () => {
+  test('(NOT YET TO BE TESTED) Success: At END state', () => {
     const player1 = playerJoinSession(session1, "Thomas");
     expect(player1).toStrictEqual({
       content: {
@@ -142,15 +152,15 @@ describe('playerJoinSession', () => {
 
     expect(playerStatus).toStrictEqual({
       content: {
-        state: "LOBBY",
-        numQuestions: 1,
+        state: "END",
+        numQuestions: 2,
         atQuestion: 0
       },
       statusCode: 200
     })
   })
   
-  test('Success: At FINAL_RESULTS state', () => {
+  test('(NOT YET TO BE TESTED) Success: At FINAL_RESULTS state', () => {
     const player1 = playerJoinSession(session1, "Thomas");
     expect(player1).toStrictEqual({
       content: {
@@ -166,12 +176,49 @@ describe('playerJoinSession', () => {
 
     expect(playerStatus).toStrictEqual({
       content: {
-        state: "LOBBY",
-        numQuestions: 1,
+        state: "FINAL_RESULTS",
+        numQuestions: 2,
         atQuestion: 0
       },
       statusCode: 200
     })
+  })
+
+  test('(NOT YET TO BE TESTED) Success: At question 2', () => {
+    const player1 = playerJoinSession(session1, "Thomas");
+    expect(player1).toStrictEqual({
+      content: {
+        playerId: expect.any(Number)
+      },
+      statusCode: 200
+    })
+    
+    const sessionStatus = adminQuizGetSessionStatus(user1, quiz1.quizId, session1).content;
+
+    const nextQ = adminQuizSessionStateUpdate(user1, quiz1.quizId, session1, "NEXT_QUESTION");
+    
+    jest.useFakeTimers();
+    
+    const skip = adminQuizSessionStateUpdate(user1, quiz1.quizId, session1, "SKIP_COUNTDOWN");
+        
+    expect(nextQ.statusCode).toBe(200);
+    expect(skip.statusCode).toBe(200);
+    
+    nextQ = adminQuizSessionStateUpdate(user1, quiz1.quizId, session1, "NEXT_QUESTION");
+
+    expect(nextQ.statusCode).toBe(200);
+
+    const status = playerStatus(player1.content.playerId);
+    
+    expect(playerStatus).toStrictEqual({
+      content: {
+        state: "QUESTION_COUNTDOWN",
+        numQuestions: 2,
+        atQuestion: 2
+      },
+      statusCode: 200
+    })
+    jest.useRealTimers()
   })
 
 })

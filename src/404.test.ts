0bdc76ba1b2d6a7df21db704df38fbd6cae22ac7
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2023-11-16 01:17:13 +1100

    change port

--- src/404.test.ts
+++ src/404.test.ts
@@ -3,9 +3,9 @@ import { port, url } from './config.json';
 
 const SERVER_URL = `${url}:${port}`;
 
-describe("Error: 404", () => {
-  test("Calling non-existent route", () => {
-    const res = request("GET", SERVER_URL + '/nonexistentroute');
+describe('Error: 404', () => {
+  test('Calling non-existent route', () => {
+    const res = request('GET', SERVER_URL + '/nonexistentroute');
     expect(res.statusCode).toBe(404);
-  })
-})
+  });
+});

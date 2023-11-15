05c2d55257602bae1c34291ae2d673e883033db7
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2023-11-16 00:10:47 +1100

    add questions to quiz in adminQuizTransfer test

--- src/V2-tests/adminQuizTransfer.test.ts
+++ src/V2-tests/adminQuizTransfer.test.ts
@@ -9,7 +9,8 @@ import {
   adminQuizTransfer,
   adminQuizCreate,
   adminUserDetails,
-  adminQuizList
+  adminQuizList,
+  adminQuizCreateQuestion
 } from '../testWrappersV2';
 
 import { Quiz, QuizList, ReturnedToken, UserDetails } from '../types';
@@ -19,6 +20,8 @@ describe('adminQuizTransfer', () => {
   let user2: ReturnedToken;
   let user2Email: string;
   let quiz: number;
+  let questInfo;
+  let questionId: number;
 
   const invalidToken = {
     token: '-1'
@@ -45,7 +48,36 @@ describe('adminQuizTransfer', () => {
       'oth',
       'other'
     ).content as ReturnedToken;
+    
+    questInfo = {
+      question: 'What is that pokemon',
+      duration: 1,
+      points: 5,
+      answers: [
+        {
+          answer: 'Pikachu',
+          correct: true,
+        },
+        {
+          answer: 'Thomas',
+          correct: false,
+        },
+      ],
+      thumbnailUrl: 'https://as2.ftcdn.net/v2/jpg/00/97/58/97/1000_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg'
+    };
 
+    questionId = (
+      adminQuizCreateQuestion(
+        user,
+        quiz,
+        questInfo.question,
+        questInfo.duration,
+        questInfo.points,
+        questInfo.answers,
+        questInfo.thumbnailUrl
+      ).content as { questionId: number }
+    ).questionId;
+    expect(questionId).toStrictEqual(expect.any(Number))
     user2Email = (adminUserDetails(user2).content as UserDetails).user.email;
   });
 
@@ -129,8 +161,13 @@ describe('adminQuizTransfer', () => {
   });
   
   test("Error: All sessions for this quiz must be in END state", () => {
-    const session1 = adminQuizSessionStart(user, quiz, 3).content.sessionId;
-    expect(session1).toStrictEqual(expect.any(Number));
+    const session1 = adminQuizSessionStart(user, quiz, 3);
+    expect(session1).toStrictEqual({
+      statusCode: 200, 
+      content: {
+        sessionId: expect.any(Number)
+      }
+    });
 
     expect(adminQuizTransfer(quiz, user, user2Email)).toStrictEqual({
       content: {

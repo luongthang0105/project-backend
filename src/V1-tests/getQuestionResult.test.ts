d7fb3d626ded6f8d1ea8a6c57504d833837f9f31
Han <hanhhan2705@gmail.com>
2023-11-17 14:44:20 +1100

    run lint fix

--- src/V1-tests/getQuestionResult.test.ts
+++ src/V1-tests/getQuestionResult.test.ts
@@ -1,4 +1,4 @@
-import { adminQuizCreate, adminQuizCreateQuestion } from "../testWrappersV2";
+import { adminQuizCreate, adminQuizCreateQuestion } from '../testWrappersV2';
 
 import {
   adminAuthRegister,
@@ -8,13 +8,13 @@ import {
   playerJoinSession,
   getQuestionResult,
   playerSubmission,
-} from "../testWrappersV1";
-import { Question, Quiz, ReturnedToken } from "../types";
+} from '../testWrappersV1';
+import { Question, Quiz, ReturnedToken } from '../types';
 
-import { expect, test } from "@jest/globals";
-import { sleepSync } from "./sleepSync";
+import { expect, test } from '@jest/globals';
+import { sleepSync } from './sleepSync';
 
-describe("getQuestionResult", () => {
+describe('getQuestionResult', () => {
   let duration: number;
   let user1: ReturnedToken;
   let quiz1: Quiz;
@@ -24,33 +24,33 @@ describe("getQuestionResult", () => {
   beforeEach(() => {
     clear();
     duration = 2;
-    user1 = adminAuthRegister("han@gmai.com", "2705uwuwuwu", "Han", "Hanh")
+    user1 = adminAuthRegister('han@gmai.com', '2705uwuwuwu', 'Han', 'Hanh')
       .content as ReturnedToken;
-    quiz1 = adminQuizCreate(user1, "Quiz 1", "Description").content as Quiz;
+    quiz1 = adminQuizCreate(user1, 'Quiz 1', 'Description').content as Quiz;
     questInfo1 = {
-      question: "What is that pokemon",
+      question: 'What is that pokemon',
       duration: duration,
       points: 5,
       answers: [
         {
-          answer: "Pikachu",
+          answer: 'Pikachu',
           correct: true,
         },
         {
-          answer: "Doraemon",
+          answer: 'Doraemon',
           correct: false,
         },
         {
-          answer: "Sakura",
+          answer: 'Sakura',
           correct: true,
         },
         {
-          answer: "Sasuke",
+          answer: 'Sasuke',
           correct: false,
         },
       ],
       thumbnailUrl:
-        "https://png.pngtree.com/png-clipart/20230511/ourmid/pngtree-isolated-cat-on-white-background-png-image_7094927.png",
+        'https://png.pngtree.com/png-clipart/20230511/ourmid/pngtree-isolated-cat-on-white-background-png-image_7094927.png',
     };
     adminQuizCreateQuestion(
       user1,
@@ -63,21 +63,21 @@ describe("getQuestionResult", () => {
     ).content as Question;
 
     const questInfo2 = {
-      question: "What is that football player",
+      question: 'What is that football player',
       duration: duration,
       points: 10,
       answers: [
         {
-          answer: "Eden Hazard",
+          answer: 'Eden Hazard',
           correct: true,
         },
         {
-          answer: "Cole Palmer",
+          answer: 'Cole Palmer',
           correct: false,
         },
       ],
       thumbnailUrl:
-        "https://png.pngtree.com/png-clipart/20230511/ourmid/pngtree-isolated-cat-on-white-background-png-image_7094927.png",
+        'https://png.pngtree.com/png-clipart/20230511/ourmid/pngtree-isolated-cat-on-white-background-png-image_7094927.png',
     };
     adminQuizCreateQuestion(
       user1,
@@ -90,97 +90,97 @@ describe("getQuestionResult", () => {
     );
 
     session1 = adminQuizSessionStart(user1, quiz1.quizId, 3).content.sessionId;
-    player1 = playerJoinSession(session1, "Thomas").content.playerId;
+    player1 = playerJoinSession(session1, 'Thomas').content.playerId;
     expect(player1).toStrictEqual(expect.any(Number));
   });
 
-  test("Player ID does not exist", () => {
+  test('Player ID does not exist', () => {
     const result = getQuestionResult(player1 + 1, 1);
     expect(result).toStrictEqual({
       content: {
-        error: "Player ID does not exist",
+        error: 'Player ID does not exist',
       },
       statusCode: 400,
     });
   });
 
-  test("Question position is not valid for the session this player is in", () => {
+  test('Question position is not valid for the session this player is in', () => {
     const result = getQuestionResult(player1, 0);
     expect(result).toStrictEqual({
       content: {
         error:
-          "Question position is not valid for the session this player is in",
+          'Question position is not valid for the session this player is in',
       },
       statusCode: 400,
     });
   });
 
-  test("Question position is not valid for the session this player is in", () => {
+  test('Question position is not valid for the session this player is in', () => {
     const result = getQuestionResult(player1, 3);
     expect(result).toStrictEqual({
       content: {
         error:
-          "Question position is not valid for the session this player is in",
+          'Question position is not valid for the session this player is in',
       },
       statusCode: 400,
     });
   });
 
-  test("Session is not in ANSWER_SHOW state", () => {
-    adminQuizSessionStateUpdate(user1, quiz1.quizId, session1, "NEXT_QUESTION");
+  test('Session is not in ANSWER_SHOW state', () => {
+    adminQuizSessionStateUpdate(user1, quiz1.quizId, session1, 'NEXT_QUESTION');
     playerSubmission([0, 1], player1, 0);
     const result = getQuestionResult(player1, 1);
     expect(result).toStrictEqual({
       content: {
-        error: "Session is not in ANSWER_SHOW state",
+        error: 'Session is not in ANSWER_SHOW state',
       },
       statusCode: 400,
     });
   });
 
-  test("Session is not yet up to this question", () => {
-    adminQuizSessionStateUpdate(user1, quiz1.quizId, session1, "NEXT_QUESTION");
+  test('Session is not yet up to this question', () => {
+    adminQuizSessionStateUpdate(user1, quiz1.quizId, session1, 'NEXT_QUESTION');
     adminQuizSessionStateUpdate(
       user1,
       quiz1.quizId,
       session1,
-      "SKIP_COUNTDOWN"
+      'SKIP_COUNTDOWN'
     );
 
     sleepSync(duration - 1);
     playerSubmission([0], player1, 1);
     sleepSync(1);
 
-    adminQuizSessionStateUpdate(user1, quiz1.quizId, session1, "GO_TO_ANSWER");
+    adminQuizSessionStateUpdate(user1, quiz1.quizId, session1, 'GO_TO_ANSWER');
     const result = getQuestionResult(player1, 2);
     expect(result).toStrictEqual({
       content: {
-        error: "Session is not yet up to this question",
+        error: 'Session is not yet up to this question',
       },
       statusCode: 400,
     });
   });
 
-  test("Successful case: get question 1 result", () => {
-    adminQuizSessionStateUpdate(user1, quiz1.quizId, session1, "NEXT_QUESTION");
+  test('Successful case: get question 1 result', () => {
+    adminQuizSessionStateUpdate(user1, quiz1.quizId, session1, 'NEXT_QUESTION');
     adminQuizSessionStateUpdate(
       user1,
       quiz1.quizId,
       session1,
-      "SKIP_COUNTDOWN"
+      'SKIP_COUNTDOWN'
     );
 
     sleepSync(duration - 1);
     playerSubmission([0, 2], player1, 1);
     sleepSync(1);
 
-    adminQuizSessionStateUpdate(user1, quiz1.quizId, session1, "GO_TO_ANSWER");
+    adminQuizSessionStateUpdate(user1, quiz1.quizId, session1, 'GO_TO_ANSWER');
 
     const result = getQuestionResult(player1, 1);
     expect(result).toStrictEqual({
       content: {
         questionId: 0,
-        playersCorrectList: ["Thomas"],
+        playersCorrectList: ['Thomas'],
         averageAnswerTime: duration - 1,
         percentCorrect: 100,
       },
@@ -188,32 +188,32 @@ describe("getQuestionResult", () => {
     });
   });
 
-  test("Successful case: get question 2 result", () => {
-    adminQuizSessionStateUpdate(user1, quiz1.quizId, session1, "NEXT_QUESTION");
+  test('Successful case: get question 2 result', () => {
+    adminQuizSessionStateUpdate(user1, quiz1.quizId, session1, 'NEXT_QUESTION');
     adminQuizSessionStateUpdate(
       user1,
       quiz1.quizId,
       session1,
-      "SKIP_COUNTDOWN"
+      'SKIP_COUNTDOWN'
     );
 
     sleepSync(duration - 1);
     playerSubmission([0], player1, 1);
     sleepSync(1);
 
-    adminQuizSessionStateUpdate(user1, quiz1.quizId, session1, "NEXT_QUESTION");
+    adminQuizSessionStateUpdate(user1, quiz1.quizId, session1, 'NEXT_QUESTION');
     adminQuizSessionStateUpdate(
       user1,
       quiz1.quizId,
       session1,
-      "SKIP_COUNTDOWN"
+      'SKIP_COUNTDOWN'
     );
 
     sleepSync(duration - 1);
     playerSubmission([1], player1, 2);
     sleepSync(1);
 
-    adminQuizSessionStateUpdate(user1, quiz1.quizId, session1, "GO_TO_ANSWER");
+    adminQuizSessionStateUpdate(user1, quiz1.quizId, session1, 'GO_TO_ANSWER');
     const result = getQuestionResult(player1, 2);
     expect(result).toStrictEqual({
       content: {
@@ -226,16 +226,16 @@ describe("getQuestionResult", () => {
     });
   });
 
-  test("Successful case: No answer submission", () => {
-    adminQuizSessionStateUpdate(user1, quiz1.quizId, session1, "NEXT_QUESTION");
+  test('Successful case: No answer submission', () => {
+    adminQuizSessionStateUpdate(user1, quiz1.quizId, session1, 'NEXT_QUESTION');
     adminQuizSessionStateUpdate(
       user1,
       quiz1.quizId,
       session1,
-      "SKIP_COUNTDOWN"
+      'SKIP_COUNTDOWN'
     );
     sleepSync(duration);
-    adminQuizSessionStateUpdate(user1, quiz1.quizId, session1, "GO_TO_ANSWER");
+    adminQuizSessionStateUpdate(user1, quiz1.quizId, session1, 'GO_TO_ANSWER');
     const result = getQuestionResult(player1, 1);
     expect(result).toStrictEqual({
       content: {
@@ -250,43 +250,37 @@ describe("getQuestionResult", () => {
   });
 });
 
-describe("Longer test", () => {
-  test("Successful case: different percentCorrect ", () => {
-    let duration: number;
-  let user1: ReturnedToken;
-  let quiz1: Quiz;
-  let questInfo1: Question;
-  let session1: number;
-  let player1: number;
-  duration = 3;
-  user1 = adminAuthRegister("han@gmai.com", "2705uwuwuwu", "Han", "Hanh")
-    .content as ReturnedToken;
-  quiz1 = adminQuizCreate(user1, "Quiz 1", "Description").content as Quiz;
-  questInfo1 = {
-    question: "What is that pokemon",
-    duration: duration,
-    points: 5,
-    answers: [
-      {
-        answer: "Pikachu",
-        correct: true,
-      },
-      {
-        answer: "Doraemon",
-        correct: false,
-      },
-      {
-        answer: "Sakura",
-        correct: true,
-      },
-      {
-        answer: "Sasuke",
-        correct: false,
-      },
-    ],
-    thumbnailUrl:
-      "https://png.pngtree.com/png-clipart/20230511/ourmid/pngtree-isolated-cat-on-white-background-png-image_7094927.png",
-  };
+describe('Longer test', () => {
+  test('Successful case: different percentCorrect ', () => {
+    const duration = 3;
+    const user1 = adminAuthRegister('han@gmai.com', '2705uwuwuwu', 'Han', 'Hanh')
+      .content as ReturnedToken;
+    const quiz1 = adminQuizCreate(user1, 'Quiz 1', 'Description').content as Quiz;
+    const questInfo1 = {
+      question: 'What is that pokemon',
+      duration: duration,
+      points: 5,
+      answers: [
+        {
+          answer: 'Pikachu',
+          correct: true,
+        },
+        {
+          answer: 'Doraemon',
+          correct: false,
+        },
+        {
+          answer: 'Sakura',
+          correct: true,
+        },
+        {
+          answer: 'Sasuke',
+          correct: false,
+        },
+      ],
+      thumbnailUrl:
+      'https://png.pngtree.com/png-clipart/20230511/ourmid/pngtree-isolated-cat-on-white-background-png-image_7094927.png',
+    };
   adminQuizCreateQuestion(
     user1,
     quiz1.quizId,
@@ -297,39 +291,40 @@ describe("Longer test", () => {
     questInfo1.thumbnailUrl
   ).content as Question;
 
-  session1 = adminQuizSessionStart(user1, quiz1.quizId, 3).content.sessionId;
-  player1 = playerJoinSession(session1, "Thomas").content.playerId;
+  const session1 = adminQuizSessionStart(user1, quiz1.quizId, 3).content.sessionId;
+
+  const player1 = playerJoinSession(session1, 'Thomas').content.playerId;
+
   expect(player1).toStrictEqual(expect.any(Number));
-  
-    const player2 = playerJoinSession(session1, "Reece James").content.playerId;
-    const player3 = playerJoinSession(session1, "Thiago Silva").content
-      .playerId;
-    console.log(player2, player3);
-    adminQuizSessionStateUpdate(user1, quiz1.quizId, session1, "NEXT_QUESTION");
-    adminQuizSessionStateUpdate(
-      user1,
-      quiz1.quizId,
-      session1,
-      "SKIP_COUNTDOWN"
-    );
-    sleepSync(1);
 
-    playerSubmission([1], player2, 1); // false
-    sleepSync(1);
+  const player2 = playerJoinSession(session1, 'Reece James').content.playerId;
+  const player3 = playerJoinSession(session1, 'Thiago Silva').content
+    .playerId;
+  adminQuizSessionStateUpdate(user1, quiz1.quizId, session1, 'NEXT_QUESTION');
+  adminQuizSessionStateUpdate(
+    user1,
+    quiz1.quizId,
+    session1,
+    'SKIP_COUNTDOWN'
+  );
+  sleepSync(1);
 
-    playerSubmission([0, 2], player3, 1); // true
+  playerSubmission([1], player2, 1); // false
+  sleepSync(1);
 
-    adminQuizSessionStateUpdate(user1, quiz1.quizId, session1, "GO_TO_ANSWER");
+  playerSubmission([0, 2], player3, 1); // true
 
-    const result = getQuestionResult(player3, 1);
-    expect(result).toStrictEqual({
-      content: {
-        questionId: 0,
-        playersCorrectList: ["Thiago Silva"],
-        averageAnswerTime: 2,
-        percentCorrect: 33,
-      },
-      statusCode: 200,
-    });
+  adminQuizSessionStateUpdate(user1, quiz1.quizId, session1, 'GO_TO_ANSWER');
+
+  const result = getQuestionResult(player3, 1);
+  expect(result).toStrictEqual({
+    content: {
+      questionId: 0,
+      playersCorrectList: ['Thiago Silva'],
+      averageAnswerTime: 2,
+      percentCorrect: 33,
+    },
+    statusCode: 200,
+  });
   });
 });
